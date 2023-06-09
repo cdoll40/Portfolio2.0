d2dd56f3d069698e2edffa153a9a6c619b268702
cdoll40 <cdoll831@gmail.com>
2023-06-08 20:30:41 -0500

    react-scroll, global css

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Hero from "../components/Hero";
+import NavBar from "../components/NavBar";
+import Skills from "../components/Skills";
+import TechStack from "../components/TechStack";
+import Projects from "../components/Projects";
+import WorkHistory from "../components/WorkHistory";
 
 const HomeScreen = () => {
-	return <div>HomeScreen</div>;
+	return (
+		<div className="max-w-screen-sm mx-auto mt-20">
+			<Hero />
+			{/* <div className="divide-y divide-solid divide-slate-600"> */}
+			<NavBar />
+			<Skills />
+			<TechStack />
+			<Projects />
+			<WorkHistory />
+			{/* </div> */}
+		</div>
+	);
 };
 
 export default HomeScreen;
